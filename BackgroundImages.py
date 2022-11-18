fc9167822e7ccbb982e448a05b15b1d09f17e481
Bhargav644 <gautambhargav644@gmail.com>
2022-11-19 00:26:48 +0530

    changes

--- BackgroundImages.py
+++ BackgroundImages.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from tkinter import Tk
 from pynput.keyboard import Key, Controller
 from db import getData
 from script import getHTMLParser
